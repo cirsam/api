851b0f1a454b27a0077aa486c479710ab9dbde6f
cirsam <cirsam@hotmail.com>
2015-05-24 18:52:05 -0400

    changes to the file structure

--- public/index.php
+++ public/index.php
@@ -2,56 +2,24 @@
 require_once '../vendor/autoload.php';
 
 use App\Config as Config;
-
+?>
+<html>
+    <head>
+        <title>Open Source API Plug-in</title>
+        <link rel="shortcut icon" href="images/favicon.ico"/>
+        <link rel="stylesheet" type="text/css" media="screen, projection" href="css/style.css" />
+        <script src="js/jquery-2.1.4.min.js"></script>
+  </head>
+<body>
+<?php
 if (isset($_REQUEST['method'])) {
     new Config\Route($_REQUEST['method']);	
 } else {
     echo '
-	DataSet = [
-	{
-		id:1,
-		color: "red",
-		value: "#f00"
-	},
-	{
-		id:2,
-		color: "green",
-		value: "#0f0"
-	},
-	{
-		id:3,
-		color: "blue",
-		value: "#00f"
-	},
-	{
-		id:4,
-		color: "cyan",
-		value: "#0ff"
-	},
-	{
-		id:5,
-		color: "magenta",
-		value: "#f0f"
-	},
-	{
-		id:6,
-		color: "yellow",
-		value: "#ff0"
-	},
-	{
-		id:7,
-		color: "black",
-		value: "#000"
-	}
-]
-		<style>
-		    ul li{list-style:none;margin-bottom:20px;}
-		    ul li strong{color:blue;}		
-		</style>
+        <h1>Open Source Plug-in API</h1>
 		<br />
 		<br />
-		<br />
-	    To use this application, start by specifying an endpoint. eg
+	    To use this application, start by specifying an endpoint. For example
 	    <ul>
 			<li><strong>get/1</strong> will get data with id = 1</li>
 			<li><strong>post/headers</strong> will insert data, still constructing it</li>
@@ -60,3 +28,6 @@ if (isset($_REQUEST['method'])) {
 		</ul>
 	';
 }
+?>
+</body>
+</html>
